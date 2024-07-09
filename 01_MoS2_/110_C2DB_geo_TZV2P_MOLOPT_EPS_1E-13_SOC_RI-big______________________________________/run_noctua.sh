dbc949172f7c107bb3ae908bf2c1a9571516002e
JWilhelm <jan.wilhelm@ur.de>
2024-07-09 09:39:46 +0200

    remove dir 75, add dir 110

--- 01_MoS2_/110_C2DB_geo_TZV2P_MOLOPT_EPS_1E-13_SOC_RI-big______________________________________/run_noctua.sh
+++ 01_MoS2_/110_C2DB_geo_TZV2P_MOLOPT_EPS_1E-13_SOC_RI-big______________________________________/run_noctua.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -t 104:10:00
+#SBATCH -t 15:10:00
 #SBATCH --exclusive
 #SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=8
-#SBATCH -N 20
+#SBATCH -N 40
 #SBATCH -J "WS2"
-#SBATCH --account="hpc-prf-optgw"
+#SBATCH --account=hpc-prf-optgw
 
 export OMP_NUM_THREADS=8
 export OMP_PLACES=cores
@@ -14,4 +14,4 @@ export OMP_PROC_BIND=true
 module reset
 module load chem/CP2K/2024.1-foss-2023b-gcc-openmpi-openblas-libgrpp
 
-srun   /pc2/groups/hpc-prf-metdyn/eprop2d1_Jan/02_compile_CP2K/46*/cp2k/exe/local/cp2k.psmp    *.inp &> cp2k.out
+srun   /pc2/groups/hpc-prf-metdyn/eprop2d1_Jan/02_compile_CP2K/48*/cp2k/exe/local/cp2k.psmp    *.inp &> cp2k.out
